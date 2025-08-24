d14bbec20bc3080f7702ed8b1cfdd79516ec61d2
Buddy119 <453058893@qq.com>
2025-08-24 22:59:52 +0800

    Revert "feat: index collection requests and add tests"
    This reverts commit dcb72dbfd717955a032314fc472075c6529ee888.

--- server/src/storage/local.storage.ts
+++ server/src/storage/local.storage.ts
@@ -1,7 +1,7 @@
 import { IStorage, PutParams } from './storage.interface';
 import { Injectable } from '@nestjs/common';
 import { createWriteStream } from 'fs';
-import * as path from 'path';
+import path from 'path';
 import fs from 'fs-extra';
 import { Readable } from 'stream';
 
